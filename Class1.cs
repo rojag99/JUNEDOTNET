9a2d165e40a628ee79af76c5aa5d640762ed0c11
rojag99 <roja.gudavalli@snaddevelopers.com>
2024-06-07 10:45:17 +0530

    commit2

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET
+{
+    public static class Class1
+    {
+        public static void p()
+        { 
+        int i = 2 + 4;
+    }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace DOTNET
 {
     internal class Program
@@ -11,6 +12,7 @@ namespace DOTNET
         static void Main(string[] args)
         {
             Console.WriteLine("KE54658578JM");
+            Class1.p();
         }
     }
 }
