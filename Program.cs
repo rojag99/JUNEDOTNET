1c001f8eca8901fb689f7b12847a0934afeae3d2
rojag99 <roja.gudavalli@snaddevelopers.com>
2024-06-07 10:39:12 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KEWHFJM");
+        }
+    }
+}
