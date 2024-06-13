a04e2c2582f0bab2b6a1a83b482596e6e8a31901
rojag99 <roja.gudavalli@snaddevelopers.com>
2024-06-13 13:27:16 +0530

    till db creation

---
+++ DOTNETPROJECTS/EmployeeAdminPortal/EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public  string? Email { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Salary { get; set; }
+    }
+}
--- DOTNETPROJECTS/EmployeeAdminPortal/EmployeeAdminPortal/Program.cs
+++ DOTNETPROJECTS/EmployeeAdminPortal/EmployeeAdminPortal/Program.cs
@@ -1,4 +1,7 @@
 
+using EmployeeAdminPortal.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace EmployeeAdminPortal
 {
     public class Program
@@ -13,6 +16,8 @@ namespace EmployeeAdminPortal
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
